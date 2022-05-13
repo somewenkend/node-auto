6b163124b7b1ce539c67ba4f9fb0736742ee5f47
sunyunpeng <yunpeng.sun@fhcsh.com>
2022-05-13 22:06:41 +0800

    添加基础组件维护页面

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/setting', function(req, res, next) {
+  res.render('setting');
 });
 
 module.exports = router;
